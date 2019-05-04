881ddef8d885b07fb11e549f4075f5f0ecc75bd9
zf <820674670@qq.com>
2019-05-04 11:40:15 +0800

    批量插入百万级数据,单线程，多线程，多线程优化

--- src/main/java/com/myproject/utils/MyThreadOne.java
+++ src/main/java/com/myproject/utils/MyThreadOne.java
@@ -26,59 +26,59 @@ public class MyThreadOne extends Thread {
         this.users = users;
     }
 
-    @Override
-    public void run() {
-        long beginTime = System.currentTimeMillis();
-        String url = "jdbc:mysql://127.0.0.1:3306/myproject";
-        String name = "com.mysql.jdbc.Driver";
-        String user = "root";
-        String password = "root";
-        Connection conn = null;
-        try {
-            Class.forName(name);
-            conn = DriverManager.getConnection(url, user, password);//获取连接
-            conn.setAutoCommit(false);//关闭自动提交，不然conn.commit()运行到这句会报错
-        } catch (ClassNotFoundException e1) {
-            e1.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // sql前缀
-        String prefix = "INSERT INTO t_sys_user (id,password,username) VALUES ";
-        try {
-            // 保存sql后缀
-            StringBuffer suffix = new StringBuffer();
-            // 设置事务为非自动提交
-            conn.setAutoCommit(false);
-            // 比起st，pst会更好些
-            PreparedStatement pst = (PreparedStatement) conn.prepareStatement("");//准备执行语句
-            // 外层循环，总提交事务次数
-            for (int a = 0; a < 10; a++) {
-                List<SysUser> users1 = users.subList(a * 10000, (a+1)*10000);
-                // 第j次提交步长
-                for (SysUser sysUser : users1) {
-                    // 构建SQL后缀
-                    suffix.append("('" + sysUser.getId() + "','" + sysUser.getPassword() + "','" + sysUser.getUsername() + "'),");
-                }
-                // 构建完整SQL
-                String sql = prefix + suffix.substring(0, suffix.length() - 1);
-                // 添加执行SQL
-                pst.addBatch(sql);
-                // 执行操作
-                pst.executeBatch();
-                // 提交事务
-                conn.commit();
-                // 清空上一次添加的数据
-                suffix = new StringBuffer();
+        @Override
+        public void run() {
+            long beginTime = System.currentTimeMillis();
+            String url = "jdbc:mysql://127.0.0.1:3306/myproject";
+            String name = "com.mysql.jdbc.Driver";
+            String user = "root";
+            String password = "root";
+            Connection conn = null;
+            try {
+                Class.forName(name);
+                conn = DriverManager.getConnection(url, user, password);//获取连接
+                conn.setAutoCommit(false);//关闭自动提交，不然conn.commit()运行到这句会报错
+            } catch (ClassNotFoundException e1) {
+                e1.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-            // 头等连接
-            pst.close();
-            conn.close();
-            long endTime = System.currentTimeMillis();
-            System.out.println(Thread.currentThread().getName() + "花费:" + (endTime - beginTime) / 1000 + "秒");
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+            // sql前缀
+            String prefix = "INSERT INTO t_sys_user (id,password,username) VALUES ";
+            try {
+                // 保存sql后缀
+                StringBuffer suffix = new StringBuffer();
+                // 设置事务为非自动提交
+                conn.setAutoCommit(false);
+                // 比起st，pst会更好些
+                PreparedStatement pst = (PreparedStatement) conn.prepareStatement("");//准备执行语句
+                // 外层循环，总提交事务次数
+                for (int a = 0; a < 10; a++) {
+                    List<SysUser> users1 = users.subList(a * 10000, (a+1)*10000);
+                    // 第j次提交步长
+                    for (SysUser sysUser : users1) {
+                        // 构建SQL后缀
+                        suffix.append("('" + sysUser.getId() + "','" + sysUser.getPassword() + "','" + sysUser.getUsername() + "'),");
+                    }
+                    // 构建完整SQL
+                    String sql = prefix + suffix.substring(0, suffix.length() - 1);
+                    // 添加执行SQL
+                    pst.addBatch(sql);
+                    // 执行操作
+                    pst.executeBatch();
+                    // 提交事务
+                    conn.commit();
+                    // 清空上一次添加的数据
+                    suffix = new StringBuffer();
+                }
+                // 头等连接
+                pst.close();
+                conn.close();
+                long endTime = System.currentTimeMillis();
+                System.out.println(Thread.currentThread().getName() + "花费:" + (endTime - beginTime) / 1000 + "秒");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-    }
 }
